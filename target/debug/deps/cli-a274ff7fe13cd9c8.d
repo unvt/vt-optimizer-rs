2905a2789ac6cfdd79b7a0b9fa38d33865b8866d
yuiseki <yuiseki@gmail.com>
2026-01-06 16:45:18 +0900

    Add .gitignore and remove build artifacts

--- target/debug/deps/cli-a274ff7fe13cd9c8.d
+++
@@ -1,5 +0,0 @@
-/everything/src/github.com/yuiseki/tile-prune/target/debug/deps/cli-a274ff7fe13cd9c8: tests/cli.rs
-
-/everything/src/github.com/yuiseki/tile-prune/target/debug/deps/cli-a274ff7fe13cd9c8.d: tests/cli.rs
-
-tests/cli.rs:
